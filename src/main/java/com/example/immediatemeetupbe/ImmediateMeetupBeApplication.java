d4ced16a99f9bc3ff58f834a737808aff9560c6f
TaetaetaE01 <grgr589@mju.ac.kr>
2024-02-07 16:06:17 +0900

    Initial setting

---
+++ src/main/java/com/example/immediatemeetupbe/ImmediateMeetupBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.immediatemeetupbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmediateMeetupBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImmediateMeetupBeApplication.class, args);
+    }
+
+}
