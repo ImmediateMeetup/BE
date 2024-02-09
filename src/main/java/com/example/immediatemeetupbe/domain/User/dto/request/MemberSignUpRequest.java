5e6681f62714bc1185475546f021113e0910840f
Erichong7 <eirchong7@gmail.com>
2024-02-10 06:11:29 +0900

    style: 사용되지 않는 import 제거

--- src/main/java/com/example/immediatemeetupbe/domain/User/dto/request/MemberSignUpRequest.java
+++ src/main/java/com/example/immediatemeetupbe/domain/User/dto/request/MemberSignUpRequest.java
@@ -2,8 +2,6 @@ package com.example.immediatemeetupbe.domain.User.dto.request;
 
 import com.example.immediatemeetupbe.domain.User.entity.Member;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
--- src/main/java/com/example/immediatemeetupbe/global/config/security/SecurityConfig.java
+++ src/main/java/com/example/immediatemeetupbe/global/config/security/SecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
