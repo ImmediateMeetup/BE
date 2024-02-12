e2846c67e93e4a96493a73dc0337f90318a0b050
hansang-yun <gkstkddbs99@naver.com>
2024-02-12 16:23:34 +0900

    style : meetingmember로 네이밍 수정

--- src/main/java/com/example/immediatemeetupbe/domain/member_meeting/entity/MeetingMemberId.java
+++ src/main/java/com/example/immediatemeetupbe/domain/member_meeting/entity/MeetingMemberId.java
@@ -2,9 +2,9 @@ package com.example.immediatemeetupbe.domain.member_meeting.entity;
 
 import java.io.Serializable;
 
-public class MemberMeetingId implements Serializable {
+public class MeetingMemberId implements Serializable {
 
     private Long member;
     private Long meeting;
-    
+
 }
