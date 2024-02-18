1680b73bf4ca13e73e942e9863834ea41c10c570
hansang-yun <gkstkddbs99@naver.com>
2024-02-18 18:20:22 +0900

    refactor : MeetingRepo -> ParticipantRepo 변경

--- src/main/java/com/example/immediatemeetupbe/domain/meeting/service/MeetingService.java
+++ src/main/java/com/example/immediatemeetupbe/domain/meeting/service/MeetingService.java
@@ -1,14 +1,22 @@
 package com.example.immediatemeetupbe.domain.meeting.service;
 
+import com.example.immediatemeetupbe.domain.meeting.dto.MeetingDto;
 import com.example.immediatemeetupbe.domain.meeting.dto.request.MeetingModifyRequest;
 import com.example.immediatemeetupbe.domain.meeting.dto.request.MeetingRegisterRequest;
+import com.example.immediatemeetupbe.domain.meeting.dto.response.MeetingListResponse;
 import com.example.immediatemeetupbe.domain.meeting.dto.response.MeetingResponse;
 import com.example.immediatemeetupbe.domain.meeting.entity.Meeting;
+import com.example.immediatemeetupbe.domain.participant.entity.Participant;
+import com.example.immediatemeetupbe.domain.member.entity.Member;
 import com.example.immediatemeetupbe.global.exception.BaseException;
+import com.example.immediatemeetupbe.global.jwt.AuthUtil;
 import com.example.immediatemeetupbe.repository.MeetingRepository;
+import com.example.immediatemeetupbe.repository.ParticipantRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 import static com.example.immediatemeetupbe.global.exception.BaseExceptionStatus.NO_EXIST_MEETING;
 
@@ -17,10 +25,19 @@ import static com.example.immediatemeetupbe.global.exception.BaseExceptionStatus
 public class MeetingService {
 
     private final MeetingRepository meetingRepository;
+    private final ParticipantRepository participantRepository;
+    private final AuthUtil authUtil;
 
     @Transactional
     public void register(MeetingRegisterRequest meetingRegisterRequest) {
-        meetingRepository.save(meetingRegisterRequest.toEntity());
+        // 방을 만든 사람
+        Member member = authUtil.getLoginMember();
+        Meeting meeting = meetingRepository.save(meetingRegisterRequest.toEntity());
+
+        participantRepository.save(Participant.builder()
+            .meeting(meeting)
+            .member(member)
+            .build());
     }
 
     @Transactional
@@ -29,7 +46,8 @@ public class MeetingService {
             throw new BaseException(NO_EXIST_MEETING.getMessage());
         }
         Meeting meeting = meetingRepository.getById(meetingModifyRequest.getId());
-        meeting.update(meetingModifyRequest.getTitle(), meetingModifyRequest.getContent(), meetingModifyRequest.getFirstDay(), meetingModifyRequest.getLastDay());
+        meeting.update(meetingModifyRequest.getTitle(), meetingModifyRequest.getContent(),
+            meetingModifyRequest.getFirstDay(), meetingModifyRequest.getLastDay());
     }
 
     @Transactional
@@ -57,5 +75,19 @@ public class MeetingService {
 
         Meeting meeting = meetingRepository.getById(id);
         meetingRepository.delete(meeting);
+    }
+
+    @Transactional(readOnly = true)
+    public MeetingListResponse getAllMeetings() {
+        Member member = authUtil.getLoginMember();
+        List<Participant> participantList = member.getParticipantList();
+
+        List<MeetingDto> meetingDtoList = participantList.stream()
+            .map(meetingMember -> MeetingDto.from(meetingMember.getMeeting()))
+            .toList();
+
+        return MeetingListResponse.builder()
+            .meetings(meetingDtoList)
+            .build();
     }
 }
