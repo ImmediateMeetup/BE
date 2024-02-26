08563b46271b7217e4195807ec977f954313af93
hansang-yun <gkstkddbs99@naver.com>
2024-02-26 23:22:05 +0900

    refactor : record 클래스에 붙은 getter 삭제

--- src/main/java/com/example/immediatemeetupbe/domain/time/dto/response/TimeTableResponse.java
+++ src/main/java/com/example/immediatemeetupbe/domain/time/dto/response/TimeTableResponse.java
@@ -3,9 +3,8 @@ package com.example.immediatemeetupbe.domain.time.dto.response;
 
 import java.time.LocalDateTime;
 import java.util.LinkedHashMap;
-import lombok.Getter;
 
-@Getter
+
 public record TimeTableResponse(LinkedHashMap<LocalDateTime, Integer> timeTable) {
 
     public static TimeTableResponse from(LinkedHashMap<LocalDateTime, Integer> timeTable) {
