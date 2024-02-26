f21a7587d6413e5d933f6b5756d9ab406f6dbf00
hansang-yun <gkstkddbs99@naver.com>
2024-02-26 22:56:02 +0900

    refactor : TimeTableResponse에 record 도입

--- src/main/java/com/example/immediatemeetupbe/domain/time/dto/response/TimeTableResponse.java
+++ src/main/java/com/example/immediatemeetupbe/domain/time/dto/response/TimeTableResponse.java
@@ -6,13 +6,7 @@ import java.util.LinkedHashMap;
 import lombok.Getter;
 
 @Getter
-public class TimeTableResponse {
-
-    private final LinkedHashMap<LocalDateTime, Integer> timeTable;
-
-    public TimeTableResponse(LinkedHashMap<LocalDateTime, Integer> timeTable) {
-        this.timeTable = timeTable;
-    }
+public record TimeTableResponse(LinkedHashMap<LocalDateTime, Integer> timeTable) {
 
     public static TimeTableResponse from(LinkedHashMap<LocalDateTime, Integer> timeTable) {
         return new TimeTableResponse(timeTable);
